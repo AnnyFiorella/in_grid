f79bd39f91ce604fbf179aca04d3dd37f7bbdac7
Rocci Escobar <rescobar@americatv.pe>
2018-05-22 13:46:27 -0500

    Añade main.css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div id="app">
+  <div id="app" >
+
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/" class="mycolor">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+
+    <!-- <v-app id="">
+      <div>
+        <v-btn color="error">Success</v-btn>
+        <v-btn color="error">Error</v-btn>
+        <v-btn color="warning">Warning</v-btn>
+        <v-btn color="info">Info</v-btn>
+      </div>
+    </v-app> -->
+
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+/*
 #nav {
   padding: 30px;
 }
@@ -27,5 +32,6 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} 
+*/
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/styles/main.css'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+ 
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
