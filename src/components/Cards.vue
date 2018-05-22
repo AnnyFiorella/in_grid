fb651fa483d0d25036a7ea295af986ed53dae032
esegur <esegura@americatv.pe>
2018-05-22 15:37:09 -0500

    cards part2

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -14,18 +14,27 @@
             <v-card color="grey lighten-2" class="black--text">
               <v-container fluid grid-list-lg>
                 <v-layout row>
-                   <v-flex xs5>
+                   <v-flex xs3>
                     <v-card-media
                       :src="card.src"
                       height="250px"
                       >
-                      
                     </v-card-media>
                   </v-flex>
-                  <v-flex xs7>
+                  <v-flex xs4>
                     <div>
                       <div class="headline black--text" v-text="card.title"></div>
-                      <div>Foster the People</div>
+                      <div><span class="black--text" v-text="card.materialCode"></span></div>
+                      <div><span class="black--text" v-text="card.programCode"></span></div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs4>
+                    <div>
+                      <div class="headline black--text" v-text="card.duration"></div>
+                      <div class="headline black--text" v-text="card.materialType"></div>
+                      <div class="headline black--text" v-text="card.materialClasification"></div>
+                      <div class="headline black--text" v-text="card.genre"></div>
+                      <div class="headline black--text" v-text="card.center"></div>                     
                     </div>
                   </v-flex>
                  
@@ -47,13 +56,22 @@ export default {
   data: () => ({
     cards: [
       {
-        title: "America Noticias: Primera Edicion",
+        title: "América Noticias: Primera Edicion",
         src: "https://cde.americatv.com.pe/minisites/an-primera-edicion-360x227-316522.jpg",
+        materialCode:123,
+        programCode:321,
+        // duration: 2h 30min,
+        // materialType:Periodistico,
+        // materialClasification:Publico General,
+        // genre: Genero1,
+        // center: Nombre del centro,
         flex: 12
       },
       {
         title: "Rapidos y Furiosos: sin control",
         src: "http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/04/fast-and-furious-8.jpg?itok=vgmJwfIs",
+        materialCode:125,
+        programCode:320,
         flex: 12
       }
     ]
