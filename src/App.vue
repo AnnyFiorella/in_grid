bb7e01984d8db0250eff16c7e737426e55ea3416
Rocci Escobar <rescobar@americatv.pe>
2018-05-22 15:28:58 -0500

    Añade estilos generales al main.css

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,25 @@
 <template>
   <div id="app" >
 
+    <AppNav/>
+
     <div id="nav">
-      <router-link to="/" class="mycolor">Home</router-link> |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
 
-    <!-- <v-app id="">
-      <div>
-        <v-btn color="error">Success</v-btn>
-        <v-btn color="error">Error</v-btn>
-        <v-btn color="warning">Warning</v-btn>
-        <v-btn color="info">Info</v-btn>
-      </div>
-    </v-app> -->
-
     <router-view/>
+
   </div>
 </template>
 
-<style>
-/*
-#nav {
-  padding: 30px;
-}
+<script>
+// @ is an alias to /src
+import AppNav from '@/components/AppNav.vue'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  components: {
+    AppNav
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} 
-*/
-</style>
+</script>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card flat>
+    <v-container fluid>
+      <v-layout row child-flex wrap>
+        <!-- Logo -->
+        <div>
+          <img :width="55" src="../assets/images/logo.png">
+        </div>
+        <!-- Menu -->
+        <div style="flex-basis: 10%">
+          <v-toolbar class="bg-nav">
+            <!-- <v-spacer></v-spacer> -->
+            <v-toolbar-items class="white--text">
+              <v-btn flat>Materiales</v-btn>
+              <v-btn flat>Programas</v-btn>
+              <v-btn flat>Segmentos</v-btn>
+              <v-btn flat>Soporte</v-btn>
+              <v-btn flat>Parrilla</v-btn>
+              <v-btn flat>Vistas</v-btn>
+              <v-btn flat>Salir</v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+        </div>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AppNav',
+  props: {
+    msg: String
+  }
+}
+</script>
