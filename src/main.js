2579557e073732ae78473fb8f6d86d125743b1c5
esegur <esegura@americatv.pe>
2018-05-22 14:12:47 -0500

    subiendo material

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
