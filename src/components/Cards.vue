91f675fe0447e2a66feb4aaaeb11411a74b9eaaa
esegur <esegura@americatv.pe>
2018-05-22 18:31:11 -0500

    añadiendo datos a cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,13 +5,11 @@
     >
       <v-container fluid grid-list-md>
           <v-layout row wrap>
-            <v-flex xs12
+            <v-flex xs12 mb-5
               v-for="card in cards"
               v-bind="{ [`xs${card.flex}`]: true }"
-              :key="card.title"
-            >
-            
-            <v-card color="grey lighten-2" class="black--text">
+              :key="card.title">
+            <v-card color="grey lighten-2" class="black--text elevation-7">
               <v-container fluid grid-list-lg>
                 <v-layout row>
                    <v-flex xs3>
@@ -21,20 +19,22 @@
                       >
                     </v-card-media>
                   </v-flex>
-                  <v-flex xs4>
-                    <div>
-                      <div class="headline black--text" v-text="card.title"></div>
-                      <div><span class="black--text" v-text="card.materialCode"></span></div>
-                      <div><span class="black--text" v-text="card.programCode"></span></div>
+                  <v-flex xs4 >
+                    <div class="pa-5 outline">
+                      <div class ="title-styles" v-text="card.title"></div>
+                      <div class= "mt-4 pa-1 bg-subtitle subtitle-styles white--text">Cod. Material:
+                         <span class="font-general white--text" v-text="card.materialCode"></span></div>
+                      <div class ="subtitle-styles">Cod. Programa: <span class="font-general black--text" v-text="card.programCode"></span></div>
                     </div>
                   </v-flex>
                   <v-flex xs4>
                     <div>
-                      <div class="headline black--text" v-text="card.duration"></div>
-                      <div class="headline black--text" v-text="card.materialType"></div>
-                      <div class="headline black--text" v-text="card.materialClasification"></div>
-                      <div class="headline black--text" v-text="card.genre"></div>
-                      <div class="headline black--text" v-text="card.center"></div>                     
+                      <div class ="subtitle-styles"><span class="underline">Duración:</span> <span class="font-general black--text" v-text="card.duration"></span></div> 
+                      <hr>                     
+                      <div class ="subtitle-styles"><span class="underline">Tipo:</span> <span class="font-general black--text" v-text="card.materialType"></span></div>
+                      <div class ="subtitle-styles"><span class="underline">Género:</span> <span class="font-general black--text" v-text="card.genre"></span></div>
+                      <hr>
+                      <div class ="subtitle-styles"><span class="underline">Centro de Costo:</span> <span class="font-general black--text" v-text="card.center"></span></div>                     
                     </div>
                   </v-flex>
                  
@@ -60,11 +60,11 @@ export default {
         src: "https://cde.americatv.com.pe/minisites/an-primera-edicion-360x227-316522.jpg",
         materialCode:123,
         programCode:321,
-        // duration: 2h 30min,
-        // materialType:Periodistico,
-        // materialClasification:Publico General,
-        // genre: Genero1,
-        // center: Nombre del centro,
+        duration: "2h 30min",
+        materialType:"Periodístico",
+        materialClasification:"Publico General",
+        genre: "Genero1",
+        center: "Nombre del centro",
         flex: 12
       },
       {
@@ -72,6 +72,11 @@ export default {
         src: "http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/04/fast-and-furious-8.jpg?itok=vgmJwfIs",
         materialCode:125,
         programCode:320,
+        duration: "2h 30min",
+        materialType:"Periodistico",
+        materialClasification:"Publico General",
+        genre: "Genero1",
+        center: "Nombre del centro",
         flex: 12
       }
     ]
